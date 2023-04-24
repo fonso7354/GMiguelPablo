55327c7610b3192ef847cf5eb5fab044ac4589a8
pablo <pablo@DESKTOP-1IRT5B8>
2023-04-24 18:54:15 +0200

    remoteCommit5

--- PMiguelPablo/src/packMiguelPablo/Conflicto.java
+++ PMiguelPablo/src/packMiguelPablo/Conflicto.java
@@ -4,7 +4,11 @@ public class Conflicto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< GMiguelPablo
 		System.out.println("Sin Conflicto");
+=======
+
+>>>>>>> b6132e8 remoteCommit5
 	}
 
 }

b83606f7309f0355fd2b5d9791deea215eba3cb1
pablo <pablo@DESKTOP-1IRT5B8>
2023-04-25 11:12:56 +0200

    commitTrunk1

---
+++ PMiguelPablo/src/packMiguelPablo/TrunkMod.java
@@ -0,0 +1,10 @@
+package packMiguelPablo;
+
+public class TrunkMod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Trunk!");
+	}
+
+}

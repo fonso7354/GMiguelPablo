409cd9be1af18828a36a25e9087f0124356808c9
Gorilla <Gorilla@Gorila>
2023-04-25 11:06:54 +0200

    commitNewBranch2

--- PMiguelPablo/src/packMiguelPabloNew/HolaMundoNew.java
+++ PMiguelPablo/src/packMiguelPabloNew/HolaMundoNew.java
@@ -3,5 +3,6 @@ package packMiguelPabloNew;
 public class HolaMundoNew {
 	public static void main(String[] args) {
 		System.out.println("Hola de nuevo, Mundo!");
+		System.out.println("Adios, pesado.");
 	}
 }

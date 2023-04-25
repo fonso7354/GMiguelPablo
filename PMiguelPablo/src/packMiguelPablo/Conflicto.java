bdda05f28f817af8081fd12da56eaad379c94d53
pablo <pablo@DESKTOP-1IRT5B8>
2023-04-25 10:41:38 +0200

    arreglo

--- PMiguelPablo/src/packMiguelPablo/Conflicto.java
+++ PMiguelPablo/src/packMiguelPablo/Conflicto.java
@@ -4,11 +4,7 @@ public class Conflicto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< GMiguelPablo
 		System.out.println("Sin Conflicto");
-=======
-
->>>>>>> b6132e8 remoteCommit5
 	}
 
 }
